17fba9a714b5b25ae1f05361b5fd1e22f50d0757
NyxDragon03 <kduque107@gmail.com>
2024-07-23 18:27:14 -0500

    funciones y optimizar codigo

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML =  'Juego del número secreto';
+//acceder a etiqueta html por medio de js de  manera individual//
+/*let titulo = document.querySelector('h1');
+titulo.innerHTML =  'Juego del número secreto';*/
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número entre el 1 y 10'
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto)
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    alert('Click desde el botón');
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+}
+
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Indica un número entre 1 y 10');
