259d5c32b021c1009824012a5646c0b48b94b65f
NyxDragon03 <kduque107@gmail.com>
2024-07-24 17:06:59 -0500

    intentos y reiniciar

--- app.js
+++ app.js
@@ -3,25 +3,54 @@
 titulo.innerHTML =  'Juego del número secreto';*/
 
 let numeroSecreto = generarNumeroSecreto();
+let contadorIntentos = 1;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Indica un número entre 1 y 10');
 
 function verificarIntento() {
     let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log(typeof(numeroUsuario));
-    console.log(numeroUsuario);
-    console.log(typeof(numeroSecreto));
-    console.log(numeroSecreto);
-    console.log(numeroUsuario === numeroSecreto);
+    if(numeroUsuario === numeroSecreto){
+        asignarTextoElemento('p', `Acertaste el número en ${contadorIntentos} ${(contadorIntentos === 1) ? 'intento' : 'intentos'}!`);
+        document.getElementById('reiniciar').removeAttribute('disabled')
+    }else{
+        if(numeroUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El número secreto es menor');
+        }else{
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        contadorIntentos++;
+        limpiarImput();
+    }
     return;
 }
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
 }
+
+function limpiarImput(){
+    // let valorCaja = document.querySelector('#valorUsuario');
+    // valorCaja.value = '';
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Indica un número entre 1 y 10');
+    numeroSecreto = generarNumeroSecreto();
+    contadorIntentos = 1;
+}
+function reiniciarJuego(){
+    //limpiar caja
+    limpiarImput();
+    //indicar msj de intervalo//generar num aleatorio//iniciar conteo de intentos
+    condicionesIniciales();
+    //deshabilitar btn de new game
+    document.querySelector('#reiniciar').setAttribute('disabled','true')
+}
+
+condicionesIniciales();
