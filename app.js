a25a1468c5dcf730dc7c6ce219bb4f2888fc4a64
NyxDragon03 <kduque107@gmail.com>
2024-07-23 20:25:20 -0500

    acceso por id, integración juego y desafío 1

--- app.js
+++ app.js
@@ -3,22 +3,25 @@
 titulo.innerHTML =  'Juego del número secreto';*/
 
 let numeroSecreto = generarNumeroSecreto();
-console.log(numeroSecreto)
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Indica un número entre 1 y 10');
 
 function verificarIntento() {
-    alert('Click desde el botón');
+    let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(typeof(numeroUsuario));
+    console.log(numeroUsuario);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroSecreto);
+    console.log(numeroUsuario === numeroSecreto);
     return;
 }
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
 }
-
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Indica un número entre 1 y 10');
